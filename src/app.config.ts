d622c531c9f5a55fd240c845f83a9c034087bd16
Yahir Rangel <yahirandres.rangel@gmail.com>
2026-04-22 18:55:09 -0500

    Initial commit

---
+++ src/app.config.ts
@@ -0,0 +1,15 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
+import Aura from '@primeuix/themes/aura';
+import { providePrimeNG } from 'primeng/config';
+import { appRoutes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+    providers: [
+        provideRouter(appRoutes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
+        provideHttpClient(withFetch()),
+        provideZonelessChangeDetection(),
+        providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } })
+    ]
+};
