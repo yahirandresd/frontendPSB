7ce5257ca074c17594c05d4864a1ef6f0d77fdc7
Yahir Rangel <yahirandres.rangel@gmail.com>
2026-05-23 16:20:47 -0500

    feat: módulo paso-limpieza con CRUD anidado bajo programa

---
+++ src/app/features/limpieza/paso-limpieza/paso-limpieza.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { PasoLimpiezaListComponent } from './pages/paso-limpieza-list/paso-limpieza-list.component';
+import { PasoLimpiezaCreateComponent } from './pages/paso-limpieza-create/paso-limpieza-create.component';
+import { PasoLimpiezaEditComponent } from './pages/paso-limpieza-edit/paso-limpieza-edit.component';
+
+export default [
+    { path: '',           component: PasoLimpiezaListComponent   },
+    { path: 'crear',      component: PasoLimpiezaCreateComponent },
+    { path: ':id/editar', component: PasoLimpiezaEditComponent   }
+] as Routes;
