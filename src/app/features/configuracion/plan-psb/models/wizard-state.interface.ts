6d433efbea3869437f9062c109bfcae9369ce88f
Yahir Rangel <yahirandres.rangel@gmail.com>
2026-05-23 16:20:31 -0500

    refactor: simplificar wizard PSB a 2 pasos (empresa + confirmación)

--- src/app/features/configuracion/plan-psb/models/wizard-state.interface.ts
+++ src/app/features/configuracion/plan-psb/models/wizard-state.interface.ts
@@ -1,10 +1,6 @@
 import { Empresa } from '../../empresa/models/empresa.interface';
-import { TipoAlimento } from '../../tipo-alimento/models/tipo-alimento.interface';
-import { PlanPSB } from './plan-psb.interface';
 
 export interface WizardState {
     empresa: Partial<Empresa>;
-    tiposAlimento: Partial<TipoAlimento>[];
-    planPsb: Partial<PlanPSB>;
     pasoActual: number;
 }
