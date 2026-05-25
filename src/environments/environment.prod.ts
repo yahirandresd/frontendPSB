223497eebd117cd53ae5c0f2b7b7ec99ccc66550
Yahir Rangel <yahirandres.rangel@gmail.com>
2026-05-25 14:53:09 -0500

    casi conectado con el back

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:3000/api',
+    apiUrl: 'http://localhost:3000',
     supabaseUrl: 'https://xdmzhkdcjlmbhcijcydj.supabase.co',
     supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkbXpoa2RjamxtYmhjaWpjeWRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NDM1NzAsImV4cCI6MjA5MzQxOTU3MH0.frtH1PDmgYKbuCzDMNFSBAdTU2nE1eO8tyV_9f9T7G8'
 };
